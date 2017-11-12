f602a0df546725d51a1e9281da26138a2ab1b453
李明阳 <limingyang@limingyangdeMacBook-Pro.local>
2017-11-12 12:32:32 -0500

    WWWW

--- src/main/java/com/somoplay/artonexpress/service/UserService.java
+++ src/main/java/com/somoplay/artonexpress/service/UserService.java
@@ -37,16 +37,22 @@ public class UserService {
 
     private final PasswordEncoder passwordEncoder;
 
-    private final SocialService socialService;
+//    private final SocialService socialService;
 
     private final AuthorityRepository authorityRepository;
 
-    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, SocialService socialService, AuthorityRepository authorityRepository) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.socialService = socialService;
-        this.authorityRepository = authorityRepository;
-    }
+//    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, SocialService socialService, AuthorityRepository authorityRepository) {
+//        this.userRepository = userRepository;
+//        this.passwordEncoder = passwordEncoder;
+//        this.socialService = socialService;
+//        this.authorityRepository = authorityRepository;
+//    }
+public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder, AuthorityRepository authorityRepository) {
+    this.userRepository = userRepository;
+    this.passwordEncoder = passwordEncoder;
+//    this.socialService = socialService;
+    this.authorityRepository = authorityRepository;
+}
 
     public Optional<User> activateRegistration(String key) {
         log.debug("Activating user for activation key {}", key);
@@ -188,7 +194,7 @@ public class UserService {
 
     public void deleteUser(String login) {
         userRepository.findOneByLogin(login).ifPresent(user -> {
-            socialService.deleteUserSocialConnection(user.getLogin());
+//            socialService.deleteUserSocialConnection(user.getLogin());
             userRepository.delete(user);
             log.debug("Deleted User: {}", user);
         });
